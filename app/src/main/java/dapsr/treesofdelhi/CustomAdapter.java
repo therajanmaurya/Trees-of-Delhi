e6df53468fa4d1e37a0a1fa04e8f84fd6c22fc68
Rajan Maurya <rajanmaurya154@gmail.com>
2015-04-02 13:46:08 +0530

    inital commit

---
+++ app/src/main/java/dapsr/treesofdelhi/CustomAdapter.java
@@ -0,0 +1,92 @@
+package dapsr.treesofdelhi;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Provide views to RecyclerView with data from mDataSet.
+ */
+public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder> {
+
+
+    private String[] mDataSet;
+    private int icons[];
+    private Context con;
+
+
+    /**
+     * Provide a reference to the type of views that you are using (custom ViewHolder)
+     */
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private final ImageView imageView;
+        private final TextView textView;
+
+
+        public ViewHolder(View v) {
+            super(v);
+
+
+            imageView = (ImageView) v.findViewById(R.id.image);
+            textView  = (TextView) v.findViewById(R.id.textView2);
+
+        }
+
+        public ImageView getImageView() {
+            return imageView;
+        }
+
+        public TextView getTextView(){
+            return textView;
+        }
+
+
+    }
+
+
+    /**
+     * Initialize the dataset of the Adapter.
+     *  @param dataSet String[] containing the data to populate views to be used by RecyclerView.
+     * @param icon
+     * @param context
+     */
+    public CustomAdapter(String[] dataSet, int[] icon, Context context) {
+        mDataSet = dataSet;
+        icons = icon;
+        this.con = context;
+    }
+
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        // Create a new view.
+        View v = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.text_row_item, viewGroup, false);
+
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, final int position) {
+
+
+        /*
+        *
+        * initializing the ImageView and TextView with data
+        *
+        * */
+        viewHolder.getImageView().setImageResource(icons[position]);
+        viewHolder.getTextView().setText(mDataSet[position]);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return icons.length;
+    }
+
+}
---
+++ app/src/main/java/dapsr/treesofdelhi/MainActivity.java
@@ -0,0 +1,145 @@
+package dapsr.treesofdelhi;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+
+public class MainActivity extends ActionBarActivity implements RecyclerItemClickListner.OnItemClickListener{
+
+    private Toolbar toolbar;
+    private RecyclerView mRecyclerView;
+    private RecyclerView.LayoutManager mLayoutManager ;
+    protected CustomAdapter mAdapter;
+    protected String[] mDataset;
+    private static final int SPAN_COUNT = 2;
+    private int icon [] = {R.drawable.ic_abstract,R.drawable.ic_abstract,R.drawable.ic_abstract,R.drawable.ic_abstract,R.drawable.ic_abstract,R.drawable.ic_abstract,R.drawable.ic_abstract,R.drawable.ic_abstract,R.drawable.ic_abstract,R.drawable.ic_abstract };
+
+    @Override
+    public void onItemClick(View childView, int position) {
+
+
+            Toast.makeText(this,"Item Number " + position,Toast.LENGTH_SHORT).show();
+
+
+    }
+
+    @Override
+    public void onItemLongPress(View childView, int position) {
+
+    }
+
+    private enum LayoutManagerType {
+        GRID_LAYOUT_MANAGER,
+        LINEAR_LAYOUT_MANAGER
+    }
+
+    Context context;
+
+    protected LayoutManagerType mCurrentLayoutManagerType;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initDataset();
+
+        /*
+        *
+        * Toolbar implementation
+        * */
+        toolbar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+
+        /*
+        *
+        * RecyclerView implementation
+        *
+        * */
+
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+
+        /*
+        *
+        * RecyclerView Item Click Event
+        *
+        * */
+        mRecyclerView.addOnItemTouchListener(new RecyclerItemClickListner(this, this));
+
+
+       // mRecyclerView.setHasFixedSize(true);
+
+        /*
+        *
+        * for Linear layout
+        *
+        * mLayoutManager = new LinearLayoutManager(this);
+        *
+        * for Grid Layout
+        *
+        * mLayoutManager = new GridLayoutManager(this,SPAN_COUNT);
+        * // SPAN_COUNT is the number of column in grid view
+        *
+        * */
+       // mLayoutManager = new LinearLayoutManager(this);
+        mLayoutManager = new GridLayoutManager(this,SPAN_COUNT);
+        mCurrentLayoutManagerType = LayoutManagerType.GRID_LAYOUT_MANAGER;
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        int scrollPosition = 0;
+        mRecyclerView.scrollToPosition(scrollPosition);
+
+        // specify an adapter (see also next example)
+        mAdapter = new CustomAdapter(mDataset , icon,context);
+        // Set CustomAdapter as the adapter for RecyclerView.
+        mRecyclerView.setAdapter(mAdapter);
+
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    private void initDataset() {
+        mDataset = new String[icon.length];
+
+        for (int i = 0; i < icon.length; i++) {
+
+            mDataset[i] = "Tree Number " + i;
+        }
+    }
+
+}
