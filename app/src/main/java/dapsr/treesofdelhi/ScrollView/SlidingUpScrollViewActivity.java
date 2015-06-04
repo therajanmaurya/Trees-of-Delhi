a46948cc7b28d765f35fcb5c7a72f2bde9941781
Rajan Maurya <rajanmaurya154@gmail.com>
2015-06-04 14:45:55 +0530

    tool up down textview problem fix

--- app/src/main/java/dapsr/treesofdelhi/ScrollView/SlidingUpScrollViewActivity.java
+++ app/src/main/java/dapsr/treesofdelhi/ScrollView/SlidingUpScrollViewActivity.java
@@ -1,5 +1,8 @@
 package dapsr.treesofdelhi.ScrollView;
 
+import android.view.View;
+import android.widget.TextView;
+
 import com.github.ksoichiro.android.observablescrollview.ObservableScrollView;
 import com.github.ksoichiro.android.observablescrollview.ObservableScrollViewCallbacks;
 import com.github.ksoichiro.android.observablescrollview.ScrollState;
@@ -11,7 +14,9 @@ import dapsr.treesofdelhi.R;
  */
 public class SlidingUpScrollViewActivity extends SlidingUpBaseActivity<ObservableScrollView> implements ObservableScrollViewCallbacks {
 
+    private static final int SLIDING_STATE_MIDDLE = 1;
     private static final int SLIDING_STATE_BOTTOM = 2;
+    ObservableScrollView scrollView;
 
     @Override
     protected int getLayoutResId() {
@@ -20,7 +25,7 @@ public class SlidingUpScrollViewActivity extends SlidingUpBaseActivity<Observabl
 
     @Override
     protected ObservableScrollView createScrollable() {
-        ObservableScrollView scrollView = (ObservableScrollView) findViewById(R.id.scroll);
+        scrollView = (ObservableScrollView) findViewById(R.id.scroll);
         scrollView.setScrollViewCallbacks(this);
         return scrollView;
     }
@@ -29,12 +34,21 @@ public class SlidingUpScrollViewActivity extends SlidingUpBaseActivity<Observabl
     public void onBackPressed() {
 
 
+
+
+        TextView container = (TextView)findViewById(R.id.container);
+
         if (SlidingUpBaseActivity.getActionbarStatus == 7){
             finish();
         }else if(SlidingUpBaseActivity.getActionbarStatus == 6 ){
-            changeSlidingState(SLIDING_STATE_BOTTOM, true);
+              changeSlidingState(SLIDING_STATE_BOTTOM, true);
         }else if(SlidingUpBaseActivity.getActionbarStatus == 5){
+             
+            scrollView.fullScroll(View.FOCUS_UP);
+            changeSlidingState(SLIDING_STATE_MIDDLE, true);
+            container.invalidate();
             changeSlidingState(SLIDING_STATE_BOTTOM, true);
+
         }
 
     }
